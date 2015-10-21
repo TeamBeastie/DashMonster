c327292a4cf27b30d4363418c3e4e5a345adbc08
Erik P. Hansen <eph@erikphansen.com>
2015-10-21 09:16:40 -0700

    failing to get a Meteor Method for weather fetching to work

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,30 +1,65 @@
-Template.dashboard.helpers({
-  counter: function () {
-    return Session.get('counter');
-  },
-  location: function() {
-    var l = Geolocation.currentLocation();
-    var ll = Geolocation.latLng();
-    console.log(l);
-    console.log(ll);
-    return ll.lat + "," + ll.lng;
-  }
-});
+var intervalTime;
+var intervalLocation;
 
-Template.dashboard.events({
-  'click button': function () {
-    // Session.set('counter', Session.get('counter') + 1);
-  }
-});
+var getLocation = function() {
+  console.log("called getLocation()");
+  var l = Geolocation.currentLocation();
+  var ll = Geolocation.latLng();
+  return ll.lat + "," + ll.lng;
+}
 
 Template.dashboard.onCreated(function() {
   console.log("Dashboard Template created");
-  console.log(" > `this` is:");
-  console.log(this);
+  intervalTime = setInterval(function() {
+    Session.set("now", new Date());
+  }, 1000);
 })
 
 Template.dashboard.onRendered(function() {
   console.log("Dashboard Template rendered");
-  console.log(" > `this` is:");
-  console.log(this);
 })
+
+Template.dashboard.onDestroyed(function() {
+  clearInterval(intervalTime);
+  clearInterval(intervalLocation);
+})
+
+Template.dashboard.helpers({
+  counter: function () {
+    return Session.get('counter');
+  },
+  time: function() {
+    var now = Session.get("now");
+    return now;
+  },
+  location: function() {
+    console.log("called the location() helper");
+    var ll = Geolocation.latLng();
+    // only set Session lat and lng if they have changed enough since the last time
+    if (ll) {
+      Session.set("lat", ll.lat);
+      Session.set("lng", ll.lng);
+    };
+    return Session.get("lat") + "," + Session.get("lng");
+    // return "some dumb location data";
+  },
+  weather: function() {
+    var weatherData = null;
+    Meteor.call('getWeather', Session.get("lat"), Session.get("lng"), function (error, result) {
+      console.log("result of the call to getWeather:");
+      console.log(result);
+      console.log("error of the call to getWeather:");
+      console.log(error);
+      weatherData = result;
+    });
+    console.log("weatherData is : " + weatherData);
+    return weatherData;
+  }
+});
+
+Template.dashboard.events({
+  'click button.refresh': function () {
+    console.log("clicked the REFRESH button");
+    // Session.set('counter', Session.get('counter') + 1);
+  }
+});
--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ Meteor.startup(function () {
 
 Meteor.methods({
   getWeather: function(lat, lng) {
-
+    console.log("called getWeather with ", lat, lng);
+    return "the data from getWeather!!!"
+    // make a call to the forecast api but ONLY if it's been at least five minutes since the last call
+    // TODO: have a collection of cached weather data the check first
   }
 })
