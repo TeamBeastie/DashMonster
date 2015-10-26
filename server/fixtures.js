0f20cc574d7a3fbcbcb799fd588bddbd2350c327
Erik P. Hansen <eph@erikphansen.com>
2015-10-25 20:54:17 -0700

    make some users if there are none

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,3 +1,25 @@
+if (Meteor.users.find().count() === 0) {
+  console.log("make a user");
+  var joeId = Accounts.createUser({
+    username: 'joe',
+    password: 'password',
+    email: 'joe@schmoe.com',
+    profile: {
+      name: 'Joe Schmoe'
+    }
+  });
+  var bobId = Accounts.createUser({
+    username: 'bob',
+    password: 'password',
+    email: 'bob@schmoe.com',
+    profile: {
+      name: 'Bob Schmoe'
+    }
+  });
+  console.log(joeId);
+  console.log(bobId);
+};
+
 if (Stops.find().count() === 0) {
   Stops.insert({
     line: 14,
@@ -11,3 +33,4 @@ if (Stops.find().count() === 0) {
     location: "SE Belmont and SE 30th Ave"
   });
 }
+
