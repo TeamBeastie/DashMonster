43fe877b1883ca5b8960d92ef783d6ca3395377e
Erik P. Hansen <eph@erikphansen.com>
2015-10-26 12:11:22 -0700

    add this.unblock() to Meteor methods that make API calls

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ Meteor.startup(function () {
 
 Meteor.methods({
   getWeather: function(lat, lng) {
+    this.unblock();
     // console.log("called getWeather with ", lat, lng);
     // make a call to the forecast api but ONLY if it's been at least five minutes since the last call
     // TODO: have a collection of cached weather data the check first
@@ -14,6 +15,8 @@ Meteor.methods({
   // takes a stopId and route, returns entire result of the TriMet API call
   // as JSON text.
   getArrivals: function(stopId, route) {
+    this.unblock();
+    console.log("called getArrivals for stopId", stopId, "at", new Date());
     var apiUrl = "https://developer.trimet.org/ws/v2/arrivals/json/true/locIDs/" + stopId + "/arrivals/3/appID/" + API_KEY_TRIMET;
     var info = HTTP.get(apiUrl);
     return info.content;
