b1ab5ae31ae7fff6150d671d4a820e5482b35b29
Erik P. Hansen <eph@erikphansen.com>
2015-11-03 08:56:24 -0800

    got a map showing the location lat,lng and a janky version of repositioning the marker on map drag

--- client/location.js
+++ client/location.js
@@ -1,3 +1,40 @@
-Template.location.helpers({
+var markers = [];
 
+Template.location.onCreated(function() {
+  GoogleMaps.load();
+  GoogleMaps.ready('map', function(map) {
+    // set the initial center
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    })
+    markers.push(marker);
+    google.maps.event.addListener(
+      map.instance, 'center_changed', function() {
+        // remove current marker
+        markers[0].setMap(null);
+        markers = [];
+        // add new marker at current center
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(map.instance.center.lat(), map.instance.center.lng()),
+          map: map.instance
+        })
+        markers.push(marker);
+        // console.log(map.instance.center.lat() + "," + map.instance.center.lng());
+      }
+    )
+    console.log(map);
+    console.log(map.instance);
+  })
+})
+
+Template.location.helpers({
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(this.lat, this.lng),
+        zoom: 15
+      }
+    }
+  }
 })
