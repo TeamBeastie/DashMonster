b96d8bccd530d4af7d6fd24df13fb3858247d889
Erik P. Hansen <eph@erikphansen.com>
2015-11-10 12:54:20 -0800

    small tweaks to the reminders

--- client/templates/reminders/reminder.js
+++ client/templates/reminders/reminder.js
@@ -11,28 +11,10 @@ Template.reminder.helpers({
   	}
 });
 
-Template.profile.events({
-	'submit .new-reminder-form': function(event){
-		event.preventDefault();
-		var remVar = $(event.target).find(".reminderField").val();
-		console.log(remVar);
-		Reminders.insert({
-			reminderTitle: remVar,
-			createdAt: new Date(),
-			userId: Meteor.userId() 
-		});
-
-		// event.target.titleField.value = "";
-
-		return false;
-	},
-
-});
-
 Template.remindersTemplate.events({
 	'click button.remove-reminder': function(){
 		result = window.confirm('Remove this reminder?');
-		
+
 		if (result){
 		Reminders.remove(this._id);
 		}
