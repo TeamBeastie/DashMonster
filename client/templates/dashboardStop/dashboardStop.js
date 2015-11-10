485cae102a93390eb458a5bda7fe68aa1a1e8b59
Erik P. Hansen <eph@erikphansen.com>
2015-11-10 13:17:17 -0800

    Replace `0` with `Now` for bus arrivals

--- client/templates/dashboardStop/dashboardStop.js
+++ client/templates/dashboardStop/dashboardStop.js
@@ -12,8 +12,11 @@ Template.dashboardStop.helpers({
         // return Math.max(Math.floor(seconds), 0);
         // use minutes:
         var minutes = e / 1000 / 60
-        return Math.max(Math.floor(minutes), 0);
-      })
+        minutes = Math.max(Math.floor(minutes), 0)
+        minutes = (minutes === 0 ? "Now" : minutes);
+        return minutes;
+        // return Math.max(minutes, 0);
+      });
       return theseETAs.join(", ") + " Minutes"
     } else {
       return "Getting arrivals..."
